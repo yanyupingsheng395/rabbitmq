c819edab94aaacef4df01f0f3b8ee1569ffb04dd
yanyupingsheng395 <395351080@qq.com>
2020-05-15 17:48:33 +0800

    延迟消息发送

--- tuling-reliabledelivery-deleycheck-callback/src/main/java/com/tuling/TulingReliabledeliveryDeleyCheckCallBackApplication.java
+++ tuling-reliabledelivery-deleycheck-callback/src/main/java/com/tuling/TulingReliabledeliveryDeleyCheckCallBackApplication.java
@@ -11,5 +11,4 @@ public class TulingReliabledeliveryDeleyCheckCallBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TulingReliabledeliveryDeleyCheckCallBackApplication.class, args);
 	}
-
 }
