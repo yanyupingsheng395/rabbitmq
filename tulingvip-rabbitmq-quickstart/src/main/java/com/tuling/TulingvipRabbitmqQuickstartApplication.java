08718ca269ba90f0ff0713fd945a383fa48116a3
yanyupingsheng395 <395351080@qq.com>
2020-05-15 17:27:37 +0800

    RabbitMQ快速开始。简单基础

--- tulingvip-rabbitmq-quickstart/src/main/java/com/tuling/TulingvipRabbitmqQuickstartApplication.java
+++ tulingvip-rabbitmq-quickstart/src/main/java/com/tuling/TulingvipRabbitmqQuickstartApplication.java
@@ -8,7 +8,6 @@ public class TulingvipRabbitmqQuickstartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TulingvipRabbitmqQuickstartApplication.class, args);
-
 	}
 
 }
